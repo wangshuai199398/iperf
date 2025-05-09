b37c86e44602553aa7983948333075beb2f8d0a7
wangs <wangs@yusur.tech>
2025-05-09 11:51:05 +0800

    59

--- src/iperf_api.c
+++ src/iperf_api.c
@@ -4454,7 +4454,7 @@ iperf_common_sockopts(struct iperf_test *test, int s)
 
     /* Set IP TOS */
     if (test->debug) {
-        printf("%s: IPV6_TCLASS iperf_common_sockopts: %d\n", __func__, IPV6_TCLASS, test->settings->tos);
+        printf("%s: IPV6_TCLASS %d iperf_common_sockopts: %d\n", __func__, IPV6_TCLASS, test->settings->tos);
     if ((opt = test->settings->tos)) {
 	    if (getsockdomain(s) == AF_INET6) {
 #ifdef IPV6_TCLASS
