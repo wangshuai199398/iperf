409ad396ea5e1d7f1b514e384479bd257eefebf0
wangs <wangs@yusur.tech>
2025-05-12 14:41:19 +0800

    60

--- src/iperf_tcp.c
+++ src/iperf_tcp.c
@@ -86,7 +86,7 @@ iperf_tcp_send(struct iperf_stream *sp)
     int r;
 
     if (sp->test->debug) {
-        printf("%s: pending_size %d\n", __func__, sp->pending_size);
+        printf("%s: pending_size %d sp->settings->blksize %d\n", __func__, sp->pending_size, sp->settings->blksize);
     }
     if (!sp->pending_size)
 	    sp->pending_size = sp->settings->blksize;
