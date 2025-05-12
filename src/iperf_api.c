93858f65c24e1dcc773e3e446d4e8781f36f3d6a
wangs <wangs@yusur.tech>
2025-05-12 11:59:50 +0800

    60

--- src/iperf_api.c
+++ src/iperf_api.c
@@ -1919,7 +1919,7 @@ iperf_send_mt(struct iperf_stream *sp)
     int no_throttle_check;
 
     if (test->debug)
-        printf("%s: test->settings->burst %d test->multisend %d\n", __func__, test->settings->burst, test->multisend);
+        printf("%s: test->settings->burst %d test->multisend %d test->settings->rate %d\n", __func__, test->settings->burst, test->multisend, test->settings->rate);
     /* Can we do multisend mode? */
     if (test->settings->burst != 0)
         multisend = test->settings->burst;
